0ce0082430d92123baf51899fd11e4456b0b9765
qiaoqiling <qiaoqiling@hztianque.com>
2019-09-26 20:10:17 +0800

    feature: add edit mode

--- miniprogram/pages/calendar/calendar.js
+++ miniprogram/pages/calendar/calendar.js
@@ -5,42 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    date:new Date(),
+    date: new Date(),
     year: 0,
     month: 0,
     dayInMonth: 0,
-    days:0,
-    editMode:true,
+    days: 0,
+    editMode: false,
     week: ['日', '一', '二', '三', '四', '五', '六'],
-    buttons: [{ text: '取消' }, { text: '确定' }],
-    targetDays:[],
-    resultDays:[]
+    buttons: [{
+      text: '取消'
+    }, {
+      text: '确定'
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     // TODO 设置默认今天
     let date = new Date()
     let year = date.getFullYear()
     let month = date.getMonth() + 1
     let dayInMonth = date.getDate()
     let days = this.refreshDays(year, month, dayInMonth)
+    
+    this.targetDays = []
+    this.resultDays = []
     this.setData({
       date,
       year,
@@ -50,13 +55,13 @@ Page({
     })
   },
 
-  refreshDays:function(year,month,dayInMonth){
+  refreshDays: function(year, month, dayInMonth) {
     let key = `${year}-${month}`
-    if (typeof this.cacheMonthMap === 'undefined'){
+    if (typeof this.cacheMonthMap === 'undefined') {
       this.cacheMonthMap = {}
     }
     let days = this.cacheMonthMap[key]
-    if(typeof days === 'undefined'){
+    if (typeof days === 'undefined') {
       let tempDate = new Date(year, month, 0)
       let dayCount = tempDate.getDate()
       tempDate.setDate(1)
@@ -64,9 +69,9 @@ Page({
       days = new Array(dayCount + firstDayInWeek).fill('').map((item, index) => {
         let day = index - firstDayInWeek + 1
         let date = `${year}-${month}-${day}`
-        let select 
+        let select
         if (this.data.editMode) {
-          if (Array.isArray(this.tempDatas) && this.tempDatas.find(s => s.date === date)){
+          if (Array.isArray(this.tempDatas) && this.validateTempData(date)) {
             select = true
           } else {
             select = false
@@ -74,13 +79,18 @@ Page({
         } else {
           select = day === dayInMonth
         }
-        return { day, select, date }
+        return {
+          day,
+          select,
+          date,
+          mills: new Date(date).getTime()
+        }
       })
       this.cacheMonthMap[key] = days
-    }else{
-      days.forEach(item=>{
+    } else {
+      days.forEach(item => {
         if (this.data.editMode) {
-          if (Array.isArray(this.tempDatas) && this.tempDatas.find(s => s.date === item.date)){
+          if (Array.isArray(this.tempDatas) && this.validateTempData(item.date)) {
             item.select = true
           } else {
             item.select = false
@@ -93,58 +103,91 @@ Page({
     return days
   },
 
-  onDayClick :function(event){
+  onDayClick: function(event) {
     console.log(event)
-    let {item} = event.currentTarget.dataset
+    let {
+      item
+    } = event.currentTarget.dataset
     this.fillTempDatas(item)
     let days = this.refreshDays(this.data.year, this.data.month, item.day)
-    this.setData({days,dayInMonth:item.day})
+    this.setData({
+      days,
+      dayInMonth: item.day
+    })
   },
 
-  fillTempDatas: function (item){
+  fillTempDatas: function(item) {
     if (typeof this.tempDatas === 'undefined') {
       this.tempDatas = []
     }
-    let find = this.tempDatas.find(s => s.date === item.date)
-    if(find){
-      this.tempDatas.splice(this.tempDatas.indexOf(find),1)
+    let find = this.validateTempData(item.date)
+    if (find) {
+      this.tempDatas.splice(this.tempDatas.indexOf(find), 1)
     } else {
       this.tempDatas.push(item)
     }
   },
-
+  validateTempData:function(date){
+    let find = this.tempDatas.find(s => {
+      if(Array.isArray(s)){
+        let find = s.find(d=>d.date === date)
+        if(typeof find !== 'undefined'){
+          return true
+        }
+      }
+      return false
+    })
+  },
+  onSetting: function(event) {
+    this.tempDatas = this.targetDays
+    this.setData({
+      editMode: true
+    })
+  },
+  onCancel: function(event) {
+    this.tempDatas = []
+    this.setData({
+      editMode: false
+    })
+  },
+  onConfirm: function(event) {
+    this.targetDays = this.tempDatas
+    this.setData({
+      editMode: false
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
